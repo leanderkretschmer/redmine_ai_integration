6e86884b9d4e28ed552ab6cdf514c99438d0b5c7
leanderkretschmer <leanderkretschmer@icloud.com>
2025-11-06 15:02:20 +0100

    v0.0.9

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_ai_integration do
   name 'Redmine AI Integration Plugin'
   author 'Leander Kretschmer'
   description 'KI-gestützte Textverbesserung für Kommentare und Ticket-Beschreibungen'
-  version '0.0.8'
+  version '0.0.9'
   url 'https://github.com/leanderkretschmer/redmine_ai_integration.git'
   author_url 'https://github.com/leanderkretschmer'
 
@@ -20,14 +20,19 @@ Redmine::Plugin.register :redmine_ai_integration do
   }, partial: 'settings/settings'
 end
 
-# Routes registrieren
+# Routes registrieren - Redmine 6 kompatibel
 Rails.application.config.to_prepare do
-  RedmineApp::Application.routes.append do
-    post 'ai_rewrite/rewrite', to: 'ai_rewrite#rewrite'
-    post 'ai_rewrite/save_version', to: 'ai_rewrite#save_version'
-    get 'ai_rewrite/get_version', to: 'ai_rewrite#get_version'
-    delete 'ai_rewrite/clear_versions', to: 'ai_rewrite#clear_versions'
-    post 'ai_rewrite/test_connection', to: 'ai_rewrite#test_connection'
+  begin
+    RedmineApp::Application.routes.append do
+      post 'ai_rewrite/rewrite', to: 'ai_rewrite#rewrite', as: 'ai_rewrite_rewrite'
+      post 'ai_rewrite/save_version', to: 'ai_rewrite#save_version', as: 'ai_rewrite_save_version'
+      get 'ai_rewrite/get_version', to: 'ai_rewrite#get_version', as: 'ai_rewrite_get_version'
+      delete 'ai_rewrite/clear_versions', to: 'ai_rewrite#clear_versions', as: 'ai_rewrite_clear_versions'
+      post 'ai_rewrite/test_connection', to: 'ai_rewrite#test_connection', as: 'ai_rewrite_test_connection'
+    end
+    Rails.logger.info "AI Integration Plugin: Routes erfolgreich registriert"
+  rescue => e
+    Rails.logger.error "AI Integration Plugin: Fehler beim Registrieren der Routes: #{e.message}"
+    Rails.logger.error e.backtrace.join("\n")
   end
 end
-
