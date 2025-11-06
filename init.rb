830e774d019dc9c3f57c7b289d0164bac655e290
leanderkretschmer <leanderkretschmer@icloud.com>
2025-11-06 14:52:27 +0100

    v0.0.6

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_ai_integration do
   name 'Redmine AI Integration Plugin'
   author 'Leander Kretschmer'
   description 'KI-gestützte Textverbesserung für Kommentare und Ticket-Beschreibungen'
-  version '0.0.5'
+  version '0.0.6'
   url 'https://github.com/leanderkretschmer/redmine_ai_integration.git'
   author_url 'https://github.com/leanderkretschmer'
 
@@ -20,21 +20,14 @@ Redmine::Plugin.register :redmine_ai_integration do
   }, partial: 'settings/settings'
 end
 
-# Assets laden
-Rails.application.config.to_prepare do
-  Redmine::Plugin.find(:redmine_ai_integration).assets.each do |asset|
-    Rails.application.config.assets.precompile << asset
-  end
-end
-
 # Routes registrieren
 Rails.application.config.to_prepare do
   RedmineApp::Application.routes.append do
-    post 'ai_rewrite/rewrite', to: 'ai_rewrite#rewrite'
-    post 'ai_rewrite/save_version', to: 'ai_rewrite#save_version'
-    get 'ai_rewrite/get_version', to: 'ai_rewrite#get_version'
-    delete 'ai_rewrite/clear_versions', to: 'ai_rewrite#clear_versions'
-    post 'ai_rewrite/test_connection', to: 'ai_rewrite#test_connection'
+    match 'ai_rewrite/rewrite', to: 'ai_rewrite#rewrite', via: :post
+    match 'ai_rewrite/save_version', to: 'ai_rewrite#save_version', via: :post
+    match 'ai_rewrite/get_version', to: 'ai_rewrite#get_version', via: :get
+    match 'ai_rewrite/clear_versions', to: 'ai_rewrite#clear_versions', via: :delete
+    match 'ai_rewrite/test_connection', to: 'ai_rewrite#test_connection', via: :post
   end
 end
 
